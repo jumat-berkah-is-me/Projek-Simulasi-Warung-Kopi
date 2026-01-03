a84c534022926378f66302c57e2975e373c64bc1
Muhamad Raka Pratama <raka824pratama@gmail.com>
2026-01-03 19:19:57 +0700

    done: add constant variables

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello, World!")
+import simpy
+import random
+import statistics
 
+WAKTU_SIMULASI = 180
+JUMLAH_BARISTA = 1
+WAKTU_BUAT_PER_CUP = 2 # menit
+INTERVAL_PELANGGAN = 3 # Setiap 3 menit ada pelanggan baru
 
-if __name__ == "__main__":
-    main()
+data_laporan = []
